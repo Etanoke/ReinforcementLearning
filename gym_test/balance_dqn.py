d7a3f9ec57f76f8765a8cba892e9ef62bebc15b8
Etanoke <etanoke@users.noreply.github.com>
2017-05-13 14:18:53 +0900

    WIP: Q-Networkでの倒立振子制御を実装中
    とりあえず実装版
    性能が普通のQ-Learningより良くない

--- gym_test/balance_dqn.py
+++ gym_test/balance_dqn.py
@@ -1,21 +1,26 @@
 u"""OpenAI gymのCarPole-v0をQ-Learning（Neural Network版）で学習する"""
 import enum
+import time
 
+import gym
 import numpy as np
+
+env = gym.make('CartPole-v0')
 
 
 def get_reward(observation):
-    """環境の値から"""
+    """環境の値から報酬を計算する"""
     x, _, angle, _ = observation
     loss = abs(x) + abs(angle) * 5
+    # DQNでは報酬を1or-1にクリッピングしたほうが学習が進みやすいらしい → 後で試す
     return -loss
 
 
 class NeuralNetwork(object):
     """ニューラルネットワークの学習管理クラス"""
     INPUT_LAYER_NEURONS = 4  # 入力層ニューロン数
-    HIDDEN_LAYER_NEURONS = 10  # 隠れ層ニューロン数
-    OUTPUT_LAYER_NEURONS = 1  # 出力層ニューロン数
+    HIDDEN_LAYER_NEURONS = 20  # 隠れ層ニューロン数
+    OUTPUT_LAYER_NEURONS = 2  # 出力層ニューロン数 = Action数
 
     LEARNING_RATE = 1e-4  # 学習率
 
@@ -25,8 +30,9 @@ class NeuralNetwork(object):
             'W_INPUT': np.random.randn(self.INPUT_LAYER_NEURONS, self.HIDDEN_LAYER_NEURONS),  # 入力層 - 隠れ層間の重み
             'W_HIDDEN': np.random.randn(self.HIDDEN_LAYER_NEURONS, self.OUTPUT_LAYER_NEURONS),  # 隠れ層 - 出力層間の重み
         }
+        self.output = None
 
-    def forward(self, x_input):
+    def forward(self, x_input, should_save_output=False):
         """ネットワークを順伝搬させて出力を計算する
         
         入力層のニューロンは順に1, 2, ..., i, ...
@@ -41,7 +47,7 @@ class NeuralNetwork(object):
         ※1: シグモイド関数を使おうとも思ったが、Q関数の近似という意味では値域を0〜1に制限したりしないほうが良いのか？わからん
         """
         # 隠れ層の計算
-        # u_j = Σ_i { x_i * w_ij }
+        # u_j = Σ_i { x_i * w_ij } + bias
         u_hidden = np.dot(x_input, self.params['W_INPUT'])
         # y_j = φ_h(u_j)
         y_hidden = self.relu(u_hidden)  # 活性化関数はReLU
@@ -52,9 +58,17 @@ class NeuralNetwork(object):
         # y_k = φ_o(u_k)
         # y_output = self.sigmoid(u_output)  # 活性化関数はシグモイド関数
         y_output = u_output  # 活性化関数は恒等関数
+
+        # 誤差逆伝搬で使う出力値
+        if should_save_output:
+            self.output = {
+                'u_hidden': u_hidden,
+                'y_hidden': y_hidden,
+                'y_output': y_output
+            }
         return y_output
 
-    def back_propagation(self, x_input, u_hidden, y_hidden, y_output, target):
+    def back_propagation(self, x_input, target):
         """誤差逆伝搬でネットワークの重みを更新する
         
         誤差関数Eは、出力が連続値であるため自乗平均をとる
@@ -79,16 +93,25 @@ class NeuralNetwork(object):
               = Σ_k{ δ_output_k * w_jk * φ_h'(u_j) * x_i }
               = Σ_k{ δ_output_k * w_jk } * φ_h'(u_j) * x_i
         """
+        if self.output is None:
+            return
+        # 誤差逆伝搬では順伝搬で計算したニューロン出力値を使う
+        u_hidden = self.output['u_hidden']
+        y_hidden = self.output['y_hidden']
+        y_output = self.output['y_output']
+
         # 隠れ層 - 出力層間の重みを更新
         # 出力層の活性化関数は恒等関数なので、φ_o'(u_k) = 1
         delta_o = y_output - target
-        delta_w2 = delta_o * y_hidden
+        delta_w2 = np.outer(y_hidden, delta_o)
         self.params['W_HIDDEN'] += -self.LEARNING_RATE * delta_w2
 
         # 入力層 - 隠れ層間の重みを更新
         # φ_h'(u_j)はReLUの微分
         delta_relu = u_hidden > 0
-        delta_w1 = np.dot(delta_o, self.params['W_HIDDEN']) * delta_relu * x_input
+        # delta_w1_tmpは　Σ_k{ δ_output_k * w_jk } * φ_h'(u_j) までの計算
+        delta_w1_tmp = np.dot(self.params['W_HIDDEN'], delta_o) * delta_relu
+        delta_w1 = np.outer(x_input, delta_w1_tmp)
         self.params['W_INPUT'] += -self.LEARNING_RATE * delta_w1 
 
     @staticmethod
@@ -120,19 +143,43 @@ class Agent(object):
     u"""エージェント"""
     def __init__(self):
         self.action_value = {}  # Q値
-        self.gamma = 0.95  # 割引率γ
-        self.alpha = 0.15  # 学習率α
+        self.gamma = 0.90  # 割引率γ
+        self.alpha = 1e-4  # 学習率α
         self.epsilon = 0.15  # 探索率ε
+        self.network = NeuralNetwork()
 
-    def decide_action(self, state_no, greedy=False):
-        u"""方策に応じて行動を選択する"""
+    def decide_action(self, state, greedy=False, should_save_output=False):
+        """方策に応じて行動を選択する"""
+        # ランダムに行動を決定するのにはaction_valuesの値が必要ないが、学習のためにはネットワークを順伝搬させておく必要がある
+        action_values = self.network.forward(state, should_save_output=should_save_output)
         if not greedy and np.random.rand() < self.epsilon:
             # ε-greedyアルゴリズムにより、self.epsilonの確率で探索行動を取る
             # 確率的に適当に行動を選択する
             return self._explore()
         else:
             # 最適の行動を選択する(greedy)
-            return self._greedy(state_no)
+            return self._greedy(action_values)
+
+    def update_action_value(self, state, decided_action, reward, next_state):
+        """Q-Learningアルゴリズムでネットワークを更新する
+        
+        元のQ-Learningの更新式は
+        Q(St, At) ← (1 - η)Q(St, At) + η(Rt+1 + γ * max_a{Q(St+1, At+1))}
+        または式変形して
+        Q(St, At) ← η( Rt+1 + γ * max_a{Q(St+1, At+1)} - Q(St, At) )
+        
+        この
+        Rt+1 + γ * max_a{Q(St+1, At+1)}
+        が教師信号targetとなる
+        """
+        next_action_values = self.network.forward(next_state, should_save_output=False)
+        next_max_action_value = np.max(next_action_values)
+        target = np.array(self.network.output['y_output'])
+        # 実際に選択した行動については報酬から教師信号を計算する
+        # 　→選択しなかった行動は更新させない
+        target[decided_action.value] = reward + self.gamma * next_max_action_value
+        # 誤差逆伝搬でネットワークを更新する
+        self.network.back_propagation(state, target)
 
     @staticmethod
     def _explore():
@@ -142,13 +189,70 @@ class Agent(object):
         else:
             return Action.ACTION2
 
-    def _greedy(self, state_no):
+    @staticmethod
+    def _greedy(action_values):
         """行動価値が最も高い行動を選択する"""
-        max_q_value = float('-inf')
+        max_action_value = float('-inf')
         max_action = None
         for action in Action:
-            q_value = self.action_value[(state_no, action)]
-            if q_value > max_q_value:
-                max_q_value = q_value
+            action_value = action_values[action.value]
+            if action_value > max_action_value:
+                max_action_value = action_value
                 max_action = action
         return max_action
+
+
+def train(agent, needs_render=False):
+    steps_log = []
+    for i_episode in range(10000):
+        state = env.reset()
+        t = 0
+        for t in range(200):
+            # env.render()
+            # time.sleep(0.02)
+            # 環境の値に応じてエージェントが行動を選択する
+            action = agent.decide_action(state, should_save_output=True)
+            # エージェントの行動に応じて環境の状態が変わる
+            last_state = state
+            state, _reward, done, info = env.step(action.value)
+            # 選んだ行動と次の状態、報酬が決まったので行動価値関数を更新する(=学習)
+            reward = get_reward(state)
+            agent.update_action_value(last_state, action, reward, state)
+            if done:
+                # print("Episode finished after {} time steps".format(t+1))
+                break
+        if i_episode > 0 and i_episode % 100 == 0:
+            saikin = steps_log[-100:]
+            print('episode: {} / 平均生存step: {}steps'.format(i_episode + 1, sum(saikin) / len(saikin)))
+        if needs_render and i_episode in [1, 25, 50, 100, 200, 400, 800, 1600, 3200, 6400]:
+            evaluate(agent, 1)
+        steps_log.append(t + 1)
+
+    return agent
+
+
+def evaluate(agent, episode_num=100):
+    for i_episode in range(episode_num):
+        state = env.reset()
+        t = 0
+        for t in range(200):
+            env.render()
+            time.sleep(0.02)
+            # 環境の値に応じてエージェントが行動を選択する
+            action = agent.decide_action(state, greedy=True)
+            # エージェントの行動に応じて環境の状態が変わる
+            state, reward, done, info = env.step(action.value)
+            # print(t, get_reward(state), reward)
+            if done:
+                break
+        print('evaluate: {}step生存'.format(t + 1))
+
+if __name__ == '__main__':
+    agent = Agent()
+    try:
+        # needs_render=Trueにすると学習経過が見られる
+        agent = train(agent, needs_render=True)
+    except KeyboardInterrupt:
+        # Ctrl+Cで学習を中断して結果確認する
+        pass
+    evaluate(agent, episode_num=100)
